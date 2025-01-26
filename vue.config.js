79715a8eba50a5399f373ed013dec80eb215c392
cranko5 <125371219+cranko5@users.noreply.github.com>
2025-01-26 22:42:00 +0100

    commit 4

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,17 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const { defineConfig } = require('@vue/cli-service');
+const webpack = require('webpack');
 
-module.exports = {
+module.exports = defineConfig({
+  transpileDependencies: true,
   devServer: {
     historyApiFallback: true,
-    publicPath: process.env.NODE_ENV === 'production' ? '/ime-aplikacije/' : '/',
   },
-};
+  publicPath: process.env.NODE_ENV === 'production' ? '/ime-aplikacije/' : '/',
+  configureWebpack: {
+    plugins: [
+      new webpack.DefinePlugin({
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+      }),
+    ],
+  },
+});
